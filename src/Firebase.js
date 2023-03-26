0ad9ca497e3de37c0445a54010ee4ca54d4c3e6f
Ousmane <97252877+EasyToolsDIALLO@users.noreply.github.com>
2023-03-26 18:15:31 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQpz_3gaxVpkBYTszakDIi1BRL-kWol6c",
-  authDomain: "cinema-5fafd.firebaseapp.com",
-  projectId: "cinema-5fafd",
-  storageBucket: "cinema-5fafd.appspot.com",
-  messagingSenderId: "173734064399",
-  appId: "1:173734064399:web:29c7b4fd20105a506d0e7a",
-  measurementId: "G-ZBDBEMZJGE"
+  apiKey: <Your credentials>,
+  authDomain: <Your credentials>,
+  projectId: <Your credentials>,
+  storageBucket: <Your credentials>,
+  messagingSenderId: <Your credentials>,
+  appId: <Your credentials>,
+  measurementId: <Your credentials>
 };
 
 // Initialize Firebase
